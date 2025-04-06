f152875a539e9c8e003ff6ce46374bec80780b37
mdshadab0 <mdshadab0500@gmail.com>
2025-04-06 22:17:01 +0530

    ecf.tf

--- terraform/ecf.tf
+++ terraform/ecf.tf
@@ -13,6 +13,8 @@ resource "aws_ecs_task_definition" "medusa_task" {
   network_mode             = "awsvpc"
   cpu                      = "512"
   memory                   = "1024"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
 
   container_definitions = jsonencode([
     {
@@ -71,4 +73,25 @@ resource "aws_ecs_service" "medusa_service" {
     assign_public_ip = true
     security_groups = [aws_security_group.ecs_sg.id]
   }
+}
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
