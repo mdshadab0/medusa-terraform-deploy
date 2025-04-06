e0db4f035e12a42b3cb4f14f4dc3a2e673021a69
mdshadab0 <mdshadab0500@gmail.com>
2025-04-06 21:52:16 +0530

    security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "ecs_sg" {
   name        = "ecs-security-group"
   description = "Allow HTTP"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 9000
