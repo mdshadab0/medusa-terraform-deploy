35109328d7bb9997388ba099477c754420575637
mdshadab0 <mdshadab0500@gmail.com>
2025-04-06 21:50:10 +0530

    network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,21 +1,35 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+
+data "aws_subnets" "default_public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+
+locals {
+  selected_subnets = slice(data.aws_subnets.default_public.ids, 0, 2)
 }
 
 resource "aws_subnet" "public" {
-  count             = length(var.public_subnet_cidrs)
-  cidr_block        = var.public_subnet_cidrs[count.index]
-  vpc_id            = aws_vpc.main.id
-  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
+  count                   = length(local.selected_subnets)
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = cidrsubnet(data.aws_vpc.default.cidr_block, 8, count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = data.aws_vpc.default.id
+}
+
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = data.aws_vpc.default.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -24,7 +38,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public_assoc" {
-  count          = length(var.public_subnet_cidrs)
+  count          = length(aws_subnet.public)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
